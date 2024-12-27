0ccb08164e8fa4e86777dfba2e91eff78c9da003
Omogo Peter <155792462+onyedika-glitch@users.noreply.github.com>
2024-12-27 04:57:14 +0100

    Update Header.js

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
-                
+import Image from 'next/image';
 const Header = () => {
     return (
 <div classname="flex items-center justify-between mb-4">
  <div classname="flex items-center">
-  <img alt="Jane Reyes" classname="rounded-full w-12 h-12 mr-4" height="50" src="https://storage.googleapis.com/a1aa/image/cjRoG7WgOF6nCpaIGMWJeXUfYaMVZ9P7drNP8YG6Gx3eje3PB.jpg" width="50"/>
+  <Image alt="Jane Reyes" classname="rounded-full w-12 h-12 mr-4" height="50" src="https://storage.googleapis.com/a1aa/image/cjRoG7WgOF6nCpaIGMWJeXUfYaMVZ9P7drNP8YG6Gx3eje3PB.jpg" width="50"/>
   <div>
    <h2 classname="text-xl font-semibold">
     Jane Reyes
@@ -22,3 +22,4 @@ const Header = () => {
 };
 
 export default Header;
+
