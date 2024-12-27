660f56488ef2c04fdbc045b1485fcae9ebf63581
Omogo Peter <155792462+onyedika-glitch@users.noreply.github.com>
2024-12-27 05:56:47 +0100

    Update index.tsx

--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes, RouteProps } from 'react-router-dom';
+import {createRoot} from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes,} from 'react-router-dom';
 import HomePage from './HomePage';
 import AnotherPage from './AnotherPage';
 
@@ -15,4 +15,6 @@ const App: React.FC = () => {
     );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('app');
+const root = createRoot(container!);
+root.render(<App />);
